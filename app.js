3e7742f5aa34bda8c408b3a630a92ca4b8a858c6
JKits123 <jim.kitson@currentdevelopment.co.uk>
2025-06-26 09:28:20 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const formSection = document.getElementById("form-section");
+    formSection.innerHTML = "<p>Smart RAMS dynamic interface will render here based on hazard database.</p>";
+});
